6894c6b41f9665f405ed17422333fd0cab5cfda2
haiderbasheer99 <haydersalih999@gmail.com>
2026-02-07 21:34:32 +0300

    modified UpdatedUserProfile with MulterFile

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,13 +7,17 @@ import {
   ParseIntPipe,
   Patch,
   Query,
+  UploadedFile,
   UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { PaginationDto } from 'src/common/pagination/dto/pagination-query.dto';
 import { ActiveUser } from 'src/auth/decorator/active-user.decorator';
 import { UpdateProfileDto } from 'src/profile/dto/update-profile.dto';
 import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { File as MulterFile } from 'multer';
 
 @Controller('user')
 @ApiBearerAuth('JWT-auth') //for marking the route with swagger auth  documentaion
@@ -31,6 +35,7 @@ export class UserController {
   }
 
   @Patch('update-profile')
+  @UseInterceptors(FileInterceptor('profileImage'))
   @ApiOperation({ summary: 'Update Current User Profile' })
   @ApiResponse({
     status: 200,
@@ -38,21 +43,25 @@ export class UserController {
   })
   public UpdateUserProfile(
     @Body() profile: UpdateProfileDto,
+    @UploadedFile() file: MulterFile,
     @ActiveUser('sub') userId: number,
   ) {
+    if (file) {
+      profile.profileImage = file.path;
+    }
     return this.userService.updateUserProfile(profile, userId);
   }
 
-  @Delete(':id')
+  @Delete()
   @ApiOperation({ summary: 'Delete User by id' })
   @ApiResponse({
     status: 200,
     description: 'it will Delete User by id in the Response',
   })
   getUser(
-    @Param('id', ParseIntPipe) id: number,
+    // @Param('id', ParseIntPipe) id: number,
     @ActiveUser('sub') userId: number,
   ) {
-    return this.userService.deleteUser(id, userId);
+    return this.userService.deleteUser(userId);
   }
 }
