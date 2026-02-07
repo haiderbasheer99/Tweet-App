83a4b8dcd592d9746958546e94e42fdb55528b92
haiderbasheer99 <haydersalih999@gmail.com>
2026-02-07 21:30:08 +0300

    no chnages

--- src/otp/otp.entity.ts
+++ src/otp/otp.entity.ts
@@ -1,26 +1,33 @@
-import { User } from "src/user/user.entity";
-import { Column, CreateDateColumn, Entity, JoinColumn, ManyToMany, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
-import { OTPType } from "./type/otpType";
+import { User } from 'src/user/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToMany,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { OTPType } from './type/otpType';
 
 @Entity()
-export class Otp{
+export class Otp {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @PrimaryGeneratedColumn()
-    id: number;
+  @ManyToOne(() => User, { nullable: false })
+  @JoinColumn()
+  user: User;
 
-    @ManyToOne(() => User, {nullable: false})
-    @JoinColumn()
-    user: User;
+  @Column()
+  token: string; //gonna be hashed in database and will be used for verification
 
-    @Column()
-    token: string; //gonna be hashed in database and will be used for verification 
+  @Column({ type: 'enum', enum: OTPType })
+  type: OTPType;
 
-    @Column({type: 'enum', enum: OTPType})
-    type: OTPType;
+  @CreateDateColumn()
+  createdAt: Date;
 
-    @CreateDateColumn()
-    createdAt: Date;
-
-    @Column()
-    expiredAt: Date
+  @Column()
+  expiredAt: Date;
 }
