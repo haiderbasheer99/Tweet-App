35c738899e26df8a0f645726cf23a26a634c58ae
haiderbasheer99 <haydersalih999@gmail.com>
2026-02-07 21:35:15 +0300

    no chnages

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,55 +1,65 @@
-import { Hashtag } from "src/hashtag/hashtag.entity";
-import { Profile } from "src/profile/profile.entity";
-import { Tweet } from "src/tweet/tweet.entity";
-import { Column, Entity, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, DeleteDateColumn, OneToOne, JoinColumn, OneToMany } from "typeorm";
+import { Hashtag } from 'src/hashtag/hashtag.entity';
+import { Profile } from 'src/profile/profile.entity';
+import { Tweet } from 'src/tweet/tweet.entity';
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+  OneToOne,
+  JoinColumn,
+  OneToMany,
+} from 'typeorm';
 
 @Entity()
-export class User{
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @PrimaryGeneratedColumn()
-    id:number;
+  @Column({
+    type: 'varchar',
+    nullable: false,
+    length: 24,
+  })
+  userName: string;
 
-    @Column({
-        type: "varchar",
-        nullable: false,
-        length: 24
-    })
-    userName: string;
+  @Column({
+    type: 'varchar',
+    nullable: false,
+    length: 100,
+    unique: true,
+  })
+  email: string;
 
-    @Column({
-        type: "varchar",
-        nullable: false,
-        length: 100,
-        unique: true
-    })
-    email: string;
+  @Column({
+    type: 'varchar',
+    nullable: false,
+    length: 100,
+  })
+  password: string;
 
-    @Column({
-        type: "varchar",
-        nullable: false,
-        length: 100
-    })
-    password: string;
+  @Column({ default: 'unverified' })
+  accountStatus: 'verified' | 'unverified';
 
-    @Column({default: 'unverified'})
-    accountStatus: 'verified' | 'unverified'
+  @OneToOne(() => Profile, (profile) => profile.user, {
+    cascade: ['insert'],
+  })
+  profile?: Profile;
 
-    @OneToOne(() => Profile, (profile) => profile.user,{
-       cascade: ['insert']})
-       profile?: Profile;
+  @OneToMany(() => Hashtag, (hashtag) => hashtag.user)
+  hashtags: Hashtag[];
 
-    @OneToMany(()=> Hashtag, (hashtag) => hashtag.user)
-    hashtags: Hashtag[]   
+  @OneToMany(() => Tweet, (tweet) => tweet.user)
+  tweets: Tweet[];
 
-    @OneToMany(() => Tweet, (tweet) => tweet.user)
-    tweets: Tweet[]
+  @CreateDateColumn()
+  createdAt: Date;
 
-    @CreateDateColumn()
-    createdAt: Date;
+  @UpdateDateColumn()
+  updatedAt: Date;
 
-    @UpdateDateColumn()
-    updatedAt: Date;
-
-    @DeleteDateColumn()
-    deletedAt: Date;
+  @DeleteDateColumn()
+  deletedAt: Date;
 }
