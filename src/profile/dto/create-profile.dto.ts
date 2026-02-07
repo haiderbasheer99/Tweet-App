ef853f5e5aaca74b96c23f8aabf133a2b7265aab
haiderbasheer99 <haydersalih999@gmail.com>
2026-02-07 21:32:39 +0300

    added null type to profileImage

--- src/profile/dto/create-profile.dto.ts
+++ src/profile/dto/create-profile.dto.ts
@@ -1,33 +1,39 @@
-import { IsDate, IsOptional, IsString, MaxLength, MinLength, } from "class-validator"
-export class CreateProfileDto{
+import {
+  IsDate,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+export class CreateProfileDto {
+  @IsString({ message: 'firstName Must be a String Value..' })
+  @IsOptional()
+  @MinLength(3, { message: 'firstName Must have a minimum of 3 characters..' })
+  @MaxLength(100, {
+    message: 'firstName Must have a maxmum of 100 characters..',
+  })
+  firstName?: string;
 
-    @IsString({message: 'firstName Must be a String Value..'})
-    @IsOptional()
-    @MinLength(3, {message: 'firstName Must have a minimum of 3 characters..'})
-    @MaxLength(100, {message: 'firstName Must have a maxmum of 100 characters..'})
-    firstName?: string;
+  @IsString({ message: 'lastName Must be a String Value..' })
+  @IsOptional()
+  @MinLength(3, { message: 'firstName Must have a minimum of 3 characters..' })
+  @MaxLength(100, { message: 'firstName Must have a maxmum of 3 characters..' })
+  lastName?: string;
 
-    @IsString({message: 'lastName Must be a String Value..'})
-    @IsOptional()
-    @MinLength(3, {message: 'firstName Must have a minimum of 3 characters..'})
-    @MaxLength(100, {message: 'firstName Must have a maxmum of 3 characters..'})
-    lastName?: string;
+  @IsString({ message: 'Gender should be String Value' })
+  @IsOptional()
+  @MaxLength(10, { message: 'Gender Must have a maxmum of 10 characters..' })
+  gender?: string;
 
-    @IsString({message: 'Gender should be String Value'})
-    @IsOptional()
-    @MaxLength(10, {message: 'Gender Must have a maxmum of 10 characters..'})
-    gender?: string;
+  @IsOptional()
+  @IsDate({ message: 'DateOfBirth should be Date' })
+  dateOfBirth?: Date;
 
-    @IsOptional()
-    @IsDate({message: 'DateOfBirth should be Date'})
-    dateOfBirth?: Date;
+  @IsString({ message: 'Bio should be String' })
+  @IsOptional()
+  bio?: string;
 
-    @IsString({message: 'Bio should be String'})
-    @IsOptional()
-    bio?: string;
-
-    @IsString()
-    @IsOptional()
-    profileImage?: string
-
+  @IsString()
+  @IsOptional()
+  profileImage?: string | null; // i added | null bcz the nest validator remove this feild when i try to update it
 }
